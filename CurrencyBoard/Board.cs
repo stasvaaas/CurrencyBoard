8c7bb8d73bfb050557eaa0f2ca3dc3fdff54a5c2
stasvaaas <105922844+stasvaaas@users.noreply.github.com>
2023-10-24 15:07:54 +0300

    Add project files.

---
+++ CurrencyBoard/Board.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyBoard
+{
+
+    // "where T : struct" is used to accept only valuable types
+    public class Board<T> where T : struct
+    {
+        public string Name { get; private set; }
+
+        //constructor to get current currency name
+        public Board(string name)
+        {
+            Name = name;
+        }
+
+        private List<T> previousRates = new List<T>();
+
+        //method to change rate
+        public void ChangeRate(T rate)
+        {
+            previousRates.Add(rate);
+            Console.WriteLine($"current {Name} rate : {rate}");
+        }
+
+        //method to display rates history
+        public void PrintHistory()
+        {
+            Console.WriteLine($"{Name} rate history:");
+            foreach (T rate in previousRates)
+            {
+                Console.WriteLine(rate);
+            }    
+        }
+    }
+}
---
+++ CurrencyBoard/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CurrencyBoard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Board<int> usd = new Board<int>("USD");
+            usd.ChangeRate(10);
+            usd.ChangeRate(30);
+            usd.ChangeRate(40);
+            usd.PrintHistory();
+
+            Board<double> eur = new Board<double>("EUR");
+            eur.ChangeRate(10);
+            eur.ChangeRate(15.3);
+            eur.ChangeRate(55.2);
+            eur.PrintHistory();
+        }
+    }
+}
